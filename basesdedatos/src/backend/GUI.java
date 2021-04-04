48fe68e7068753929937405a7d42731403a7e482
Manuel Ortiz <manuel4612@hotmail.com>
2021-04-04 14:52:55 -0500

    Hotel Reservation App

---
+++ basesdedatos/src/backend/GUI.java
@@ -0,0 +1,14 @@
+/*
+
+ */
+package backend;
+
+import interfaz.grafico;
+import interfaz.principal;
+import interfaz.registro;
+
+public class GUI {
+    grafico gr = new grafico();
+    principal pr = new principal();
+    registro reg = new registro();
+}
